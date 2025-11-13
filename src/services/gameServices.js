9fcf38ff9176d93576210cf976b8131fb7b19439
RaykoRadev <christmas898@gmail.com>
2025-11-13 10:41:30 +0100

    move service tamplete

---
+++ src/services/gameServices.js
@@ -0,0 +1,35 @@
+export async function getAll() {
+    try {
+        const res = await fetch("");
+        const data = await res.json();
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+export async function getOne() {
+    try {
+        const res = await fetch("");
+        const data = await res.json();
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+export async function editOne() {
+    try {
+        const res = await fetch("");
+        const data = await res.json();
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+export async function deleteOne() {
+    try {
+        const res = await fetch("");
+        const data = await res.json();
+    } catch (err) {
+        alert(err.message);
+    }
+}
